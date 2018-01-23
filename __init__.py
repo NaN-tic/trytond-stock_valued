68f32b53ee434359196d24cad2299a28570c694a
C?dric Krier <ced@b2ck.com>
2018-01-23 15:39:05 +0100

    Fix style

--- __init__.py
+++ __init__.py
@@ -1,13 +1,13 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 from trytond.pool import Pool
-from .move import *
-from .shipment import *
+from . import move
+from . import shipment
 
 
 def register():
     Pool.register(
-        Move,
-        ShipmentIn,
-        ShipmentOut,
+        move.Move,
+        shipment.ShipmentIn,
+        shipment.ShipmentOut,
         module='stock_valued', type_='model')
