75770ce27a69a7ff1ffdb93bd9700c093c4c1a27
Raimon Esteve <raimon@nan-tic.com>
2019-08-30 14:41:40 +0200

    Add shipment out return valued and get gross unit price and taxes from product when is not valued from origin #038568 #038306

--- configuration.py
+++ configuration.py
@@ -1,7 +1,7 @@
 # This file is part of Tryton.  The COPYRIGHT file at the top level of
 # this repository contains the full copyright notices and license terms.
 from trytond.model import fields
-from trytond.pool import Pool, PoolMeta
+from trytond.pool import PoolMeta
 
 __all__ = ['Configuration']
 
@@ -9,6 +9,16 @@ __all__ = ['Configuration']
 class Configuration(metaclass=PoolMeta):
     __name__ = 'stock.configuration'
 
-    valued_sale_line = fields.Boolean('Use Valued Sale Line',
-        help='If marked valued shipment take amount from sale lines, not '
-            'outgoing move shipment')
+    valued_origin = fields.Boolean('Use Valued origin',
+        help='If marked valued shipment take amount from origin, not '
+            'from the move')
+
+    @classmethod
+    def __register__(cls, module_name):
+        table = cls.__table_handler__(module_name)
+
+        # Migration from 5.2: rename reference into valued_sale_line
+        if (table.column_exist('valued_sale_line')):
+            table.column_rename('valued_sale_line', 'valued_origin')
+
+        super(Configuration, cls).__register__(module_name)
